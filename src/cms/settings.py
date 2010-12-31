09df99d7a4c7872c1cddc27dc6a14a358549f205
Brandon R. Stoner <monokrome@monokro.me>
2010-12-31 14:43:39 -0800

    Added aliases to project settings.

--- src/cms/settings.py
+++ src/cms/settings.py
@@ -117,6 +117,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'aliases',
     'news',
     'navigation',
 )
