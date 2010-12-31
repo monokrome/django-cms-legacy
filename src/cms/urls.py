ca0bf84723a520f3249a69be407375b3825e170c
Brandon R. Stoner <monokrome@monokro.me>
2010-12-31 15:47:25 -0800

    Added settings-based index URL

--- src/cms/urls.py
+++ src/cms/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 from django.contrib import admin
 
 admin.autodiscover()
 
+# A few settings that we can use to override the index page
+index_view = getattr(settings, 'INDEX_VIEW', 'news.views.index')
+index_args = getattr(settings, 'INDEX_ARGS', {})
+
 urlpatterns = patterns('',
+    url(r'^$', index_view, index_args, name='site-index'),
+
     (r'^news/', include('news.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -11,3 +18,4 @@ urlpatterns = patterns('',
 
     (r'^admin/', include(admin.site.urls)),
 )
+
