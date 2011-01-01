e59c8c34057545ba6a11c9d2b53e5f6ce942ac20
Brandon R. Stoner <monokrome@monokro.me>
2011-01-01 10:02:20 -0800

    Added markup to project settings

--- src/cms/settings.py
+++ src/cms/settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.comments',
     'django.contrib.admin',
     'django.contrib.flatpages',
+    'django.contrib.markup',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'profiles',
