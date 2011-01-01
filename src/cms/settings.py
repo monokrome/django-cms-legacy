7e2de2d3f4b50684792d641c6eee9bf0f01f95ab
Brandon R. Stoner <monokrome@monokro.me>
2011-01-01 08:16:44 -0800

    Fixed an issue in template loaders.

--- src/cms/settings.py
+++ src/cms/settings.py
@@ -85,8 +85,6 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
-
-    asset_path('templates')
 )
 
 MIDDLEWARE_CLASSES = (
